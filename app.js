679f4d05947f94204620150f460f989ad2009865
Hajin <hajinkim@kangwon.ac.kr>
2019-05-16 23:28:12 +0900

    강의 2-5 색 변경, 채우기 기능 추가

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 500;
-canvas.height = 500;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 500;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
     painting = false;
@@ -30,15 +38,46 @@ function onMouseMove(event) {
     
 }
 
-function onMouseDown(event) {
-    painting = true;
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
+function handlRangeChange(event) {
+    ctx.lineWidth = event.target.value;
+}
 
+function handleModeClick(event) {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = "Paint";
+    } else {
+        filling = true;
+        mode.innerText = "Fill";
+    }
+}
+
+function handleCanvasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+if (range) { 
+    range.addEventListener("input", handlRangeChange);
+}
+
+if (mode) {
+    mode.addEventListener("click", handleModeClick);
 }
